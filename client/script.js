1f1d0eac86f391be3c3e6c296c1814e911d67234
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-01-01 18:17:06 +0200

    UrL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://localhost:5000', {
+    const response = await fetch('https://kiki-ai.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
